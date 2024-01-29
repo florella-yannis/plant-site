80c2e053403d40ab1bf910c426e6018feba0d562
florella-yannis <florellayannis@gmail.com>
2024-01-29 15:06:58 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/*=============== SHOW MENU ===============*/
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== QUESTIONS ACCORDION ===============*/
+const acc = document.getElementsByClassName("accordion");
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
